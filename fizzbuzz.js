daac091ad8748b496cc8fb8bd71590680234e06a
Sakar Poudel <ultimatesakar@gmail.com>
2023-04-17 13:28:10 +0545

    Added exception handling for invalid input

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,11 @@
 module.exports.fizzbuzz = function (number) {
-  if (number % 3 == 0 && number % 5 == 0) return "fizzbuzz";
-  if (number % 3 == 0) return "fizz";
-  if (number % 5 == 0) return "buzz";
-  return number;
+  try {
+    if (typeof number != "number") throw new Error("Input should be a number");
+    if (number % 3 == 0 && number % 5 == 0) return "fizzbuzz";
+    if (number % 3 == 0) return "fizz";
+    if (number % 5 == 0) return "buzz";
+    return number;
+  } catch (error) {
+    console.log(error);
+  }
 };
